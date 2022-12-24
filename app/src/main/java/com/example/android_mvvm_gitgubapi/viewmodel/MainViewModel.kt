99ffb990a2b22f7599302bf645bec8904fe62d05
jiwon2724 <stopone3119@naver.com>
2022-12-24 16:20:22 +0900

    SharedFlow 1. stateFlow보다 디테일한 설정값들을 설정 가능하다. 2. 중복값을 필터링한다. 연속된 emit이 같은값 이면 collect X
    replay : 새로운 구독자들에게 이전 이벤트 방출여부 0 = 방출, 1 = 방출X
    extraBufferCapacity : 추가 버퍼 생성 여부 1 = 생성
    onBufferOverflow : 버퍼 초과시 처리 여부
    
    사용방법은 stateFlow와 같지만, 상태가 없음!
    
    StateFlow는 SharedFlow이다.
    SharedFlow의 모든 기본 규칙, 제약 조건, 연산자 등은 StateFlow에 계승된다.
    
    다만 초기값을 가지는 것, 단 한 개의 최신 상태값을 가지는 것 그리고 SharedFlow의 resetReplayCache가 지원되지않는 것이 그 차이점이다.

--- app/src/main/java/com/example/android_mvvm_gitgubapi/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/android_mvvm_gitgubapi/viewmodel/MainViewModel.kt
@@ -6,6 +6,8 @@ import com.example.android_mvvm_gitgubapi.model.Repositories
 import com.example.android_mvvm_gitgubapi.model.UserInfo
 import com.example.android_mvvm_gitgubapi.repository.MainRepository
 import com.example.android_mvvm_gitgubapi.ui.RepositoriesUiState
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
@@ -18,6 +20,26 @@ class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
     val repositoriesUiState: StateFlow<RepositoriesUiState?>
         get() = _repositoriesUiState.asStateFlow()
 
+
+    /** sharedFlow에 관하여
+     * stateFlow보다 디테일한 설정값들을 설정 가능하다.
+     * 중복값을 필터링한다. 연속된 emit이 같은값 이면 collect X
+     *
+     *
+     * replay : 새로운 구독자들에게 이전 이벤트 방출여부 0 = 방출, 1 = 방출X
+     * extraBufferCapacity : 추가 버퍼 생성 여부 1 = 생성
+     * onBufferOverflow : 버퍼 초과시 처리 여부
+     *
+     * **/
+
+    private val _sharedFlow = MutableSharedFlow<Int>(
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val sharedFlow: SharedFlow<Int>
+        get() = _sharedFlow.asSharedFlow()
+
     /** asStateFLow()에 관하여..
      *
      * 시퀀스를 동일한 방식으로 치환하는 건 성능을 위해서
@@ -26,6 +48,13 @@ class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
      * kotlin에서 read-only는 안정성과 동치니까요
      *
      * **/
+
+    suspend fun simple() { // flow builder
+        repeat(10) {
+            _sharedFlow.emit(it)
+            Log.d("repeat : ", it.toString())
+        }
+    }
 
 
     fun getRepositories(username: String) = viewModelScope.launch {
